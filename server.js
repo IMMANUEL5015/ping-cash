d8438a188b0609909651df93e26a493c4045b909
IMMANUEL5015 <immanueldiai@gmail.com>
2021-01-05 07:10:42 +0100

    Setup Connection to Database

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 
+//Connect to database
+const URI = process.env.DATABASE_URI;
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(res => console.log('Successfully connected to database'))
+    .catch(error => console.error(console.log("Database Error", error.message)));
+
+//Setup server
 app.listen(PORT, () => {
     console.log(`Sever has started and is listening on port ${PORT}`);
 });

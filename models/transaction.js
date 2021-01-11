a59221015b3c4e1f8c5a8e29d52083750d15b2fb
IMMANUEL5015 <immanueldiai@gmail.com>
2021-01-11 21:46:21 +0100

    Upgrade the Transaction Model

--- models/transaction.js
+++ models/transaction.js
@@ -1,17 +1,71 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = mongoose.Schema({
-    transactionType: String,
-    senderFullName: String,
-    senderPhoneNumber: String,
+    transactionType: {
+        type: String,
+        required: [true, 'Please specify the transaction type.'],
+        enum: ["send-to-nigeria", "send-within-nigeria"]
+    },
+    senderFullName: {
+        type: String,
+        required: [true, 'Please specify your fullname.'],
+    },
+    senderPhoneNumber: {
+        type: String,
+        required: [true, 'Please provide your phone number.']
+    },
     accountNumberForRefund: String,
     bankForRefund: String,
-    receiverFullName: String,
-    receiverPhoneNumber: String,
-    amount: String,
+    bankSortCode: String,
+    receiverFullName: {
+        type: String,
+        required: [true, 'Please provide the full name of the receiver.']
+    },
+    receiverPhoneNumber: {
+        type: String,
+        required: [true, 'Please provide the phone number of the receiver.']
+    },
+    amount: {
+        type: String,
+        required: [true, 'Please specify an amount for this transaction.']
+    },
     chargeReceiverTheCommission: Boolean,
-    reference: String,
-    status: String
+    reference: {
+        type: String,
+        required: [true, 'Every transaction must have a unique reference.'],
+        unique: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'cancelled', 'refunded', 'received']
+    },
+    charges: {
+        type: String,
+        required: [true, 'There must be charges for every transaction.']
+    },
+    finalAmountPaid: {
+        type: String,
+        required: [true, 'What is the actual amount paid by the sender?']
+    },
+    finalAmountReceived: {
+        type: String,
+        required: [true, 'What is the actual amount to be received?']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date
+    },
+    country: {
+        type: String,
+        required: [true, 'Please specify the country you are sending money from.']
+    },
+    currency: {
+        type: String,
+        required: [true, 'Please specify the currency you are using to make the payment.']
+    }
 });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 const router = require('./routes/transaction');
+const cors = require('cors');
 
 //Connect to database
 const URI = process.env.DATABASE_URI;
@@ -15,8 +16,15 @@ mongoose.connect(URI, {
 }).then(res => console.log('Successfully connected to database'))
     .catch(error => console.error(console.log("Database Error", error.message)));
 
-app.use(express.json());    
+app.use(cors());
+app.use(express.json());
 app.use(router);
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 'fail', message: 'The resource you are looking for cannot be found.'
+    });
+});
 
 //Setup server
 app.listen(PORT, () => {

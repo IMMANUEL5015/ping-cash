6e633e79b328d8782f50733cf4645f73481a42b6
IMMANUEL5015 <immanueldiai@gmail.com>
2021-01-12 22:32:02 +0100

    Modify Function for Initializing a Transaction to Uitilize the Catch Async Function

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,31 +1,27 @@
 const Transaction = require('../models/transaction');
 const randomString = require('random-string');
+const catchAsync = require('../utils/catchAsync');
 
-exports.initializeTransaction = async (req, res, next) => {
-    try {
-        const uniqueString = randomString({ length: 6, numeric: true });
-        const transaction = await Transaction.create({
-            reference: `PNG-${uniqueString}eq`,
-            transactionType: req.body.transactionType,
-            senderFullName: req.body.senderFullName,
-            senderPhoneNumber: req.body.senderPhoneNumber,
-            accountNumberForRefund: req.body.accountNumberForRefund,
-            bankForRefund: req.body.bankForRefund,
-            bankSortCode: req.body.bankSortCode,
-            receiverFullName: req.body.receiverFullName,
-            receiverPhoneNumber: req.body.receiverPhoneNumber,
-            amount: req.body.amount,
-            chargeReceiverTheCommission: req.body.chargeReceiverTheCommission,
-            country: req.body.country,
-            currency: req.body.currency
-        });
+exports.initializeTransaction = catchAsync(async (req, res, next) => {
+    const uniqueString = randomString({ length: 6, numeric: true });
+    const transaction = await Transaction.create({
+        reference: `PNG-${uniqueString}eq`,
+        transactionType: req.body.transactionType,
+        senderFullName: req.body.senderFullName,
+        senderPhoneNumber: req.body.senderPhoneNumber,
+        accountNumberForRefund: req.body.accountNumberForRefund,
+        bankForRefund: req.body.bankForRefund,
+        bankSortCode: req.body.bankSortCode,
+        receiverFullName: req.body.receiverFullName,
+        receiverPhoneNumber: req.body.receiverPhoneNumber,
+        amount: req.body.amount,
+        chargeReceiverTheCommission: req.body.chargeReceiverTheCommission,
+        country: req.body.country,
+        currency: req.body.currency
+    });
 
-        return res.status(201).json({
-            status: 'success', message: 'Transaction Initialized Successfully',
-            transaction
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ status: 'error', message: error.message });
-    }
-}
+    return res.status(201).json({
+        status: 'success', message: 'Transaction Initialized Successfully',
+        transaction
+    });
+});

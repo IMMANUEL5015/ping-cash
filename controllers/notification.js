86dfc8883768849163853853ac0da1a90e134f7b
IMMANUEL5015 <immanueldiai@gmail.com>
2021-03-30 13:52:45 +0100

    Mark Unread Notifications as Read

--- controllers/notification.js
+++ controllers/notification.js
@@ -11,45 +11,35 @@ exports.createNotification = async (obj) => {
 }
 
 exports.unreadNotifications = catchAsync(async (req, res, next) => {
-    try {
-        const notifications = await Notification.find({
-            user: req.user._id,
-            status: 'unread'
-        }).sort('-createdAt');
+    const notifications = await Notification.find({
+        user: req.user._id,
+        status: 'unread'
+    }).sort('-createdAt');
 
-        return res.status(200).json({
-            status: 'Success',
-            notifications
-        });
-    } catch (error) {
-        console.error(error);
-    }
+    return res.status(200).json({
+        status: 'Success',
+        notifications
+    });
 });
 
 exports.markOneAsRead = catchAsync(async (req, res, next) => {
-    try {
-        await Notification.findOneAndUpdate(
-            { _id: req.params.notificationId, user: req.user._id },
-            { status: 'read' },
-            { new: true }
-        );
-        return res.status(200).json({
-            status: 'Success'
-        });
-    } catch (error) {
-        console.error(error);
-    }
+    await Notification.findOneAndUpdate(
+        { _id: req.params.notificationId, user: req.user._id },
+        { status: 'read' },
+        { new: true }
+    );
+    return res.status(200).json({
+        status: 'Success'
+    });
 });
 
-exports.markAllAsRead = async (userId) => {
-    try {
-        await Notification.updateMany(
-            { user: userId, status: 'unread' },
-            { status: 'read' }
-        );
+exports.markAllAsRead = catchAsync(async (req, res, next) => {
+    await Notification.updateMany(
+        { user: req.user._id, status: 'unread' },
+        { status: 'read' }
+    );
 
-        return 'Updated Succesfully.';
-    } catch (error) {
-        console.error(error);
-    }
-}
+    return res.status(200).json({
+        status: 'Success'
+    });
+});
--- routes/notification.js
+++ routes/notification.js
@@ -6,5 +6,6 @@ router.use(admin.protect);
 
 router.get('/unread', notification.unreadNotifications);
 router.patch('/mark_as_read/:notificationId', notification.markOneAsRead);
+router.patch('/mark_all_as_read/', notification.markAllAsRead);
 
 module.exports = router;
